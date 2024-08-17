b1f311cabf050300c1360ce8a12d12836c4c72d2
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-08-17 23:26:02 +0200

    update font

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -88,7 +88,7 @@ const Sidebar = () => {
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in">Login</Link>
+              <Link href="/sign-in">LOGIN</Link>
             </Button>
           </SignedOut>
         </nav>
